110e60eef34ec63fa99ee8d137ae51674c5d1976
AlexanderPravin <sosupesos@gmail.com>
2023-10-25 10:02:55 +0300

    Intefraces for Repository and Mapper added

---
+++ Domain.VolgaIT/Interfaces/IMapper.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Domain.VolgaIT.Interfaces
+{
+    public interface IMapper<Entity, DTO> where Entity : class where DTO : class
+    {
+        abstract static DTO CreateDTOFromEntity(Entity entity);
+        abstract static IEnumerable<DTO> CreateDTOsFromEntities(IEnumerable<Entity> entities);
+    }
+}
---
+++ Domain.VolgaIT/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.VolgaIT.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddEntityAsync(T entity);
+        void DeleteEntity(T entity);
+        void UpdateEntity(T entity);
+        Task<ICollection<T>> GetAllEntitiesAsync();
+        Task<ICollection<T>> GetEntitiesByAsync(Expression<Func<T, bool>> filter);
+        Task<T?> GetEntityByIdAsync(string id);
+        Task<T?> GetEntityByAsync(Expression<Func<T, bool>> filter);
+    }
+}
